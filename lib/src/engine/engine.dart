944f13d02d3fdd3d94cab278dbc5670fc09cfe4b
Sarah Clark <sarahclark@google.com>
2021-05-23 12:33:03 -0700

    Fix copyright, trim unused vars

--- lib/src/engine/engine.dart
+++ lib/src/engine/engine.dart
@@ -1,18 +1,17 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import 'package:statecharts/statecharts.dart';
 
 class Engine<T> {
@@ -20,22 +19,21 @@ class Engine<T> {
   final T? context;
   var _executionContext;
 
-  Iterable<RuntimeState<T>> _configuration = {};
-  var _statesToInvoke;
+  // Iterable<RuntimeState<T>> _configuration = {};
+  // var _statesToInvoke;
 
-  var _datamodel;
-  var _internalQueue;
-  var _externalQueue;
-  var _historyValue;
-  var _running;
-  var _binding;
-  var _historyValues;
+  // var _datamodel;
+  // var _internalQueue;
+  // var _externalQueue;
+  // var _historyValue;
+  // var _running;
+  // var _binding;
+  // var _historyValues;
 
   Engine(RootState<T> startNode, [this.context])
       : root = RuntimeState.wrapSubtree(startNode);
 
-  Iterable<State<T>> get activeStates =>
-      [for (var i in _configuration) i.state];
+  Iterable<State<T>> get activeStates => [];
 
   ExecutionContext<T> get executionContext => _executionContext;
 
