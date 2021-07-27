8e0b95c91be29402650b3042e0d7fe399806fbc3
Sarah Clark <sarahclark@google.com>
2021-07-27 13:41:38 -0700

    Start SCXML implementation

---
+++ scxml/lib/scxml.dart
@@ -0,0 +1,4 @@
+library scxml;
+
+export 'src/xml.dart';
+export 'src/xml.parser.dart';
---
+++ scxml/lib/src/xml.dart
@@ -0,0 +1,371 @@
+import 'package:ixp_runtime/annotations.dart';
+import 'package:logging/logging.dart';
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import 'package:meta/meta.dart';
+
+const NAMESPACE = 'http://www.w3.org/2005/07/scxml';
+
+final _log = Logger('SCXML');
+
+abstract class StateBase {
+  @alias('onentry')
+  final OnEntryElement onEntry;
+  @alias('onexit')
+  final OnExitElement onExit;
+
+  bool get isAtomic;
+
+  StateBase({@required this.onEntry, @required this.onExit});
+}
+
+@tag('scxml')
+class SCXMLElement {
+  final String initial;
+  final String name;
+  @alias('xmlns')
+  final String namespace;
+  final double version;
+  @alias('datamodel')
+  final String datamodelType;
+  final String binding;
+
+  final List<StateBase> body;
+  final DatamodelElement datamodel;
+  final String script;
+
+  SCXMLElement(this.body,
+      {this.initial,
+      this.name,
+      this.namespace = NAMESPACE,
+      this.version = 1.0,
+      this.datamodelType = 'null', // for now
+      this.binding = 'early', // enum: early, late
+      this.datamodel,
+      this.script})
+      : assert(version == 1.0, 'Version 1.0 required'),
+        assert(namespace == NAMESPACE, 'xmlns attribute'),
+        assert(body.isNotEmpty);
+}
+
+@tag('onentry')
+class OnEntryElement {
+  final List<ExecutableContent> body;
+
+  OnEntryElement(this.body);
+}
+
+@tag('onexit')
+class OnExitElement {
+  final List<ExecutableContent> body;
+
+  OnExitElement(this.body);
+}
+
+abstract class ParallelChild {}
+
+@tag('state')
+class StateElement extends StateBase with ParallelChild {
+  final String id;
+  final String initial;
+  final TransitionElement transition;
+
+  @alias('initial')
+  final InitialStateElement initialState;
+  final List<StateBase> body;
+  final HistoryElement history;
+
+  final DatamodelElement datamodel;
+  final InvokeElement invoke;
+
+  StateElement(
+      {this.id,
+      this.initial,
+      onEntry,
+      onExit,
+      this.transition,
+      this.initialState,
+      this.body,
+      this.history,
+      this.datamodel,
+      this.invoke})
+      : super(onEntry: onEntry, onExit: onExit);
+
+  @override
+  bool get isAtomic => body?.isNotEmpty ?? true;
+}
+
+@tag('parallel')
+class ParallelElement extends StateBase with ParallelChild {
+  final String id;
+  final TransitionElement transition;
+  List<ParallelChild> body;
+  final HistoryElement history;
+  final DatamodelElement datamodel;
+  final InvokeElement invoke;
+
+  @override
+  bool get isAtomic => false;
+
+  ParallelElement(
+      {this.id,
+      onEntry,
+      onExit,
+      this.transition,
+      this.history,
+      this.datamodel,
+      this.invoke})
+      : super(onEntry: onEntry, onExit: onExit);
+}
+
+@tag('initial')
+class InitialStateElement {
+  final TransitionElement transition;
+
+  InitialStateElement({@required this.transition});
+}
+
+@tag('final')
+class FinalStateElement extends StateBase {
+  final String id;
+
+  @alias('donedata')
+  final DoneDataElement doneData;
+
+  FinalStateElement({this.id, onEntry, onExit, this.doneData})
+      : super(onEntry: onEntry, onExit: onExit);
+
+  @override
+  bool get isAtomic => true;
+}
+
+@tag('transition')
+class TransitionElement {
+  final String event;
+  final String cond;
+  final String target;
+  final String type;
+
+  TransitionElement(
+      {this.event, this.cond, this.target, this.type = 'external'});
+  List<ExecutableContent> children;
+}
+
+@tag('donedata')
+class DoneDataElement {
+  final ContentElement content;
+  final List<ParamElement> params;
+
+  DoneDataElement({this.content, this.params});
+}
+
+@tag('param')
+class ParamElement {
+  final String name;
+  final String expr;
+  final String location;
+
+  ParamElement({@required this.name, this.expr, this.location});
+}
+
+@tag('content')
+class ContentElement {
+  final String expr;
+  // TODO Define @xml() annotation
+  final String body;
+
+  ContentElement({this.expr, this.body});
+}
+
+@tag('datamodel')
+class DatamodelElement {
+  final List<DataElement> data;
+
+  DatamodelElement({this.data});
+}
+
+@tag('data')
+class DataElement {
+  final String id;
+  final Uri src;
+  final String expr;
+
+  // TODO Implement contents
+
+  DataElement({@required this.id, this.src, this.expr});
+}
+
+@tag('invoke')
+class InvokeElement {
+  final Uri type;
+  @alias('typeexpr')
+  final String typeExpr;
+  final Uri src;
+  @alias('srcexpr')
+  final String srcExpr;
+  final String id;
+  @alias('idlocation')
+  final String idLocation;
+  @alias('namelist')
+  final String nameList;
+  @ifEquals('true')
+  @alias('autoforward')
+  final bool autoForward;
+
+  final ParamElement param;
+  final FinalizeElement finalize;
+  final ContentElement content;
+
+  InvokeElement(
+      {this.type,
+      this.typeExpr,
+      this.src,
+      this.srcExpr,
+      this.id,
+      this.idLocation,
+      this.nameList,
+      this.autoForward,
+      this.param,
+      this.finalize,
+      this.content});
+}
+
+@tag('finalize')
+class FinalizeElement {}
+
+@tag('history')
+class HistoryElement {
+  final String id;
+  final String type; // enum: 'deep' or 'shallow'
+  final TransitionElement transition;
+
+  HistoryElement({this.id, this.type = 'shallow', @required this.transition});
+}
+
+// -----------------------------------------
+// Executable code
+
+abstract class ExecutableContent {}
+
+@tag('raise')
+class RaiseElement implements ExecutableContent {
+  final String event;
+
+  RaiseElement({@required this.event});
+}
+
+@tag('if')
+class IfElement implements ExecutableContent {
+  final String cond;
+  final List<ExecutableContent> block;
+
+  IfElement(this.cond, this.block);
+}
+
+@tag('elseif')
+class ElseIfElement implements ExecutableContent {
+  final String cond;
+
+  ElseIfElement({@required this.cond});
+}
+
+@tag('else')
+class ElseElement implements ExecutableContent {}
+
+@tag('foreach')
+class ForEachElement implements ExecutableContent {
+  final String arraySrc;
+  final String item;
+  final String index;
+
+  ForEachElement({@required this.arraySrc, @required this.item, this.index});
+}
+
+@tag('log')
+class LogElement implements ExecutableContent {
+  final String label;
+  final String expr;
+
+  LogElement({this.label = '', this.expr});
+}
+
+@tag('assign')
+class AssignElement implements ExecutableContent {
+  final String location;
+  final String expr;
+
+  AssignElement({@required this.location, this.expr});
+
+  // TODO implement children
+}
+
+@tag('script')
+class ScriptElement implements ExecutableContent {
+  final Uri srcUri;
+  // TODO Read the raw text
+  @textElement
+  final String script;
+
+  ScriptElement(this.srcUri, this.script);
+}
+
+@tag('send')
+class SendElement implements ExecutableContent {
+  final String event;
+  @alias('eventexpr')
+  final String eventExpr;
+  final Uri target;
+  @alias('targetexpr')
+  final String targetExpr;
+  final Uri type;
+  @alias('typeexpr')
+  final String typeExpr;
+  final String id;
+  @alias('idlocation')
+  final String idLocation;
+  final String delay;
+  @alias('delayexpr')
+  final String delayExpr;
+  final String namelist;
+
+  final List<ParamElement> parameters;
+  final List<ContentElement> contents;
+
+  SendElement(
+      {this.event,
+      this.eventExpr,
+      this.target,
+      this.targetExpr,
+      this.type,
+      this.typeExpr,
+      this.id,
+      this.idLocation,
+      this.delay,
+      this.delayExpr,
+      this.namelist,
+      this.parameters,
+      this.contents});
+}
+
+@tag('cancel')
+class CancelElement implements ExecutableContent {
+  @alias('sendid')
+  final String sendId;
+  @alias('sendidexpr')
+  final String sendIdExpr;
+
+  CancelElement({this.sendId, this.sendIdExpr});
+}
