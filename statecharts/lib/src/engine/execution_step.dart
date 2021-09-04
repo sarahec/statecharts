f33608c274f98b906b080c8783b164fe086f7fd1
Sarah Clark <sarahclark@google.com>
2021-09-04 14:26:31 -0700

    Rework the step api to hide implementation details

--- statecharts/lib/src/engine/execution_step.dart
+++ statecharts/lib/src/engine/execution_step.dart
@@ -1,192 +1,50 @@
-import 'dart:collection';
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-import 'package:built_collection/built_collection.dart';
-import 'package:built_value/built_value.dart';
-import 'package:collection/collection.dart';
-import 'package:meta/meta.dart';
-import 'package:ordered_set/ordered_set.dart';
 import 'package:statecharts/statecharts.dart';
 
-part 'execution_step.g.dart';
-
 /// Contains the results of one execution step.
-///
-/// This uses [built_value](https://pub.dev/packages/built_value) to create
-/// an immutable object which, when asked to change a value, creates a
-/// modified copy. This makes debugging easier, including the ability to
-/// roll back the engine to a prior step by loading this object.
-///
-abstract class ExecutionStep<T>
-    implements Built<ExecutionStep<T>, ExecutionStepBuilder<T>> {
-  factory ExecutionStep([void Function(ExecutionStepBuilder<T>) updates]) =
-      _$ExecutionStep<T>;
+abstract class ExecutionStep<T> {
+  ExecutionStep<T> applyTransitions(Iterable<Transition<T>> transitions);
 
-  /// Generates the initial execution step using [RootState.initialTransition].
-  ///
-  /// If there's no initial transition, select the first substate under
-  /// the.
-  static ExecutionStep<T> initial<T>(RootState<T> root) {
-    // Collect all the explicit initial states
-    final initialStates = <State<T>>{};
-    for (var s in root.toIterable.where((probe) => probe.isCompound)) {
-      if (s.initialTransition == null) continue;
-      initialStates.addAll(s.initialTransition!.targetStates);
-    }
-    return ExecutionStep<T>((b) => b
-      ..root = root
-      ..selections.addAll(initialStates));
-  }
-
-  // Required by built_value
-  ExecutionStep._();
+  /// Create a new step after adding and removing states.
+  ExecutionStep<T> applyChanges({
+    Iterable<State<T>> remove = const [],
+    Iterable<State<T>> add = const [],
+  });
 
   /// All active states, including resolved history states.
   ///
   /// This rebuilds the entire tree from scratch (for now)
-  @memoized
-  Set<State<T>> get activeStates =>
-      UnmodifiableSetView<State<T>>(buildTree(selections));
+  Set<State<T>> get activeStates;
 
   /// All states that need [State.onEntry] called, in order.
-  @memoized
-  Iterable<State<T>> get entryStates =>
-      activeStates.difference(priorStep?.activeStates ?? {}).toList()
-        ..sort((a, b) => a.order - b.order);
+  Iterable<State<T>> get entryStates;
 
-  /// All states that need [State.onExit] called, in order.
-  @memoized
-  Iterable<State<T>> get exitStates => priorStep == null
-      ? []
-      : (priorStep!.activeStates.difference(activeStates).toList()
-        ..sort((a, b) => a.order - b.order));
-
-  /// History from the prior step, or an empty set if no prior step.
-  @memoized
-  BuiltMap<String, Iterable<State<T>>> get priorHistory =>
-      priorStep?.history ?? BuiltMap<String, Iterable<State<T>>>();
-
-  /// Updates the history map to include exit states that need to record
-  /// the prior active configuration (i.e. they contain a history state).
-  @memoized
-  BuiltMap<String, Iterable<State<T>>> get history {
-    if (priorStep == null) {
-      return BuiltMap<String, Iterable<State<T>>>();
-    }
-    final b = priorHistory.toBuilder();
-    for (var s in exitStates.where((s) => s.containsHistoryState)) {
-      b[s.id!] = historyValuesFor(s);
-    }
-    return b.build();
-  }
+  /// All states that need [State.onExit] called, in reverse order.
+  Iterable<State<T>> get exitStates;
 
   /// True if any values have changed in this step.
-  @memoized
-  bool get isChanged =>
-      priorStep == null ||
-      !SetEquality().equals(selections.asSet(), priorStep!.selections.asSet());
-
-  /// Link back to the previous step.
-  ExecutionStep<T>? get priorStep;
+  bool get isChanged;
 
   /// The root of the tree
   RootState<T> get root;
 
   /// Explicitly selected states (from transitions)
-  BuiltSet<State<T>> get selections;
+  Set<State<T>> get selections;
 
   /// The transitions taken.
   Iterable<Transition<T>>? get transitions;
-
-  /// Generates the history entry for a subtree.
-  @visibleForOverriding
-  Iterable<State<T>> historyValuesFor(State<T> s) {
-    assert(s.containsHistoryState);
-    // Since this is called for states that are exiting, we have to use the prior active states
-    final priorActive = priorStep?.activeStates ?? <State<T>>{};
-    final activeChildren =
-        s.substates.where((probe) => priorActive.contains(probe));
-    final historyChildren = s.substates.whereType<HistoryState>();
-    // From the spec:
-    // If the 'type' of a <history> element is "shallow", the SCXML processor
-    // must record the immediately active children of its parent before taking
-    // any transition that exits the parent. If the 'type' of a <history>
-    // element is "deep", the SCXML processor must record the active atomic
-    // descendants of the parent before taking any transition that exits the
-    // parent.
-    //
-    // Note that in a conformant SCXML document, a <state> or <parallel>
-    // element may have both "deep" and "shallow" <history> children.
-    final result = <State<T>>{};
-    for (var hs in historyChildren) {
-      if (hs.type == HistoryDepth.SHALLOW) {
-        result.addAll(activeChildren);
-      } else {
-        final deepChildren = [
-          for (var c in activeChildren) c.activeDescendents(priorActive).last
-        ];
-        result.addAll(deepChildren);
-      }
-    }
-    return result;
-  }
-
-  @visibleForOverriding
-  Iterable<State<T>> replaceHistoryStates(Iterable<State<T>> selections) {
-    final _historyStates = selections.whereType<HistoryState<T>>();
-    if (_historyStates.isEmpty) return selections;
-    final concreteStates = selections.toSet();
-    concreteStates.removeAll(_historyStates);
-    for (var h in _historyStates) {
-      concreteStates.addAll(priorHistory[h] ?? h.transition.targetStates);
-    }
-    return concreteStates;
-  }
-
-  @visibleForOverriding
-  Set<State<T>> buildTree(Iterable<State<T>> selections,
-      {State<T>? startNode}) {
-    final tree = <State<T>>{};
-    final baseNode = startNode ?? root;
-
-    // Build links back to the starting node from known states
-    void _expandSelected(_selections, State<T> top) {
-      tree.addAll(_selections);
-      for (var s in _selections) {
-        tree.addAll(s.ancestors(upTo: top));
-      }
-    }
-
-    void _addSubtree(State<T> state) {
-      var probe = state;
-
-      while (true) {
-        tree.add(probe);
-        if (probe.isAtomic) return;
-
-        // Move to the substate(s)
-        if (probe.isParallel) {
-          for (var s in probe.substates.where((s) => !tree.contains(s))) {
-            _addSubtree(s);
-          }
-        } else {
-          // Find the active substate(s)
-          final _selections = state.initialTransition?.targetStates;
-          if (_selections == null) {
-            probe = state.substates.first;
-          } else {
-            tree.addAll(buildTree(_selections, startNode: state));
-            return;
-          }
-        }
-      }
-    }
-
-    final concreteSelections = replaceHistoryStates(selections);
-    _expandSelected(concreteSelections, baseNode);
-    for (var s in concreteSelections.where((s) => s.isCompound)) {
-      _addSubtree(s);
-    }
-    tree.add(baseNode);
-    return tree;
-  }
 }
