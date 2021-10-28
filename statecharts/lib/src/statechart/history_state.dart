84ad166efbf05858be117675f79c42c65adf6527
Sarah Clark <sarahclark@google.com>
2021-10-27 20:36:31 -0700

    async not needed

--- statecharts/lib/src/statechart/history_state.dart
+++ statecharts/lib/src/statechart/history_state.dart
@@ -13,8 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:async';
-
 import 'package:statecharts/statecharts.dart';
 
 enum HistoryDepth { SHALLOW, DEEP }
